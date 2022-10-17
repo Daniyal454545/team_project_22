1d0e934670254769a25f8befaa3b9ec31e6afc03
Daniyal454545 <daniyalkozhokmatov@gmail.com>
2022-10-17 15:53:58 +0600

    first commit from daniyal

---
+++ account/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class AccountManage(BaseUserManager):
+
+    def _create_acc_(self, email):
